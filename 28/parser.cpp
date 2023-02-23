38ef7304e6dde41245fa1066b8645bc6fce9bc5a
Huzaifa <l201017@lhr.nu.edu.pk>
2023-02-24 00:07:32 +0500

    Lexical Analyzer Completed

---
+++ 28/parser.cpp
@@ -0,0 +1,53 @@
+#include "parser.h"
+
+void parser::syntax_error()
+{
+    cout << "SYNTAX ERROR\n";
+    exit(1);
+}
+token parser::expect(TokenType expected_type)
+{
+    token t = _lexer.getNextToken();
+    if (t.tokenType != expected_type)
+        syntax_error();
+    return t;
+}
+parser::parser(const char filename[])
+{
+    _lexer = lexer(filename);
+}
+void parser::readAndPrintAllInput() //read and print allinputs (provided)
+{
+    token t;
+    t = _lexer.getNextToken();
+    while (t.tokenType != TokenType::END_OF_FILE)
+    {
+        t.Print();
+        t = _lexer.getNextToken();
+    }
+}
+void parser::resetPointer()
+{
+    _lexer.resetPointer();
+}
+//this function is for sample purposes only
+/*
+bool parser::statements()
+{
+    //statements-- > COLON LPAREN start RPAREN
+    if (_lexer.peek(1).tokenType == TokenType::COLON)
+    {
+        expect(TokenType::COLON);
+        if (_lexer.peek(1).tokenType == TokenType::LPAREN)
+        {
+            expect(TokenType::LPAREN);
+            start();
+            if (_lexer.peek(1).tokenType == TokenType::RPAREN)
+            {
+                expect(TokenType::RPAREN);
+                return true;
+            }
+        }
+    }
+    return false;
+}*/ 
---
+++ 28/sample_code.ru
@@ -0,0 +1,30 @@
+kaam kaamKaNaam@adad (param1@adad | param2@adad) karo
+;yeh comment hai
+STATEMENT;ye bhi comment hai
+rakho variableName1@adad;Declaring Variable
+rakho _variableName2@adad : variableName1  + _variableName2;Declaration with Initialization of variables
+;Until Loop
+jab tak (param1 <= param2 | param1 < param2 | param1 <> param2| param1 >= param2 | param1 > param2 ) karo
+;Statements
+bas karo
+;if else if
+agar (EXPRESSION) to phir karo
+;Statements
+warna agar (EXPRESSION) to phir
+;Statements
+warna phir
+;Statements
+bas karo
+
+;Printing Statement
+
+;Input with Print Statements
+
+;Input Statements
+
+;Function Call
+rakho variableName := chalao kaamKaNaam(arg1 | arg2);
+rakho variableName@adad := chalao kaamKaNaam(arg1 | arg2);
+chalao kaamKaNaam(arg1 | arg2 | arg3);
+wapas bhaijo variableName;Return Statement
+kaam khatam
